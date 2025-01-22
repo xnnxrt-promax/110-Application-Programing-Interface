a442965b902e4dd36e61506cf0a3b6774c756bc2
xnnxrt-promax <github@serenely.day>
2025-01-22 22:42:40 +0700

    server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, 'frontend/build')));
 // API Endpoint
 app.get('/api', (req, res) => {
     res.json({
-        message: 'Hello from \nBackend!',
+        message: '<br />คุณไม่ได้รับอนุญาติให้ใช้ API นี่',
         version: '2.3.8',
         festival: 'China New Year'
     });
