969657ca1674ccfd82235b862a991d9e1a9c8660
xnnxrt-promax <github@serenely.day>
2025-01-29 19:18:19 +0700

    server.js

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.use(express.static(path.join(__dirname, 'frontend/build')));
 app.get('/api', (req, res) => {
     res.json({
         message: '',
-        version: '2.3.8',
+        version: '2.3.9',
         festival: 'China New Year',
         az_discord_profile: 'https://discord.com/users/1119676900462313572',
         doc: 'https://code-101-or-documents.gitbook.io/',
