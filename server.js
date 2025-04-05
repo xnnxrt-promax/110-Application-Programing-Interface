f1dc4dbdd0f78450ac3a39b5a99d7cfbfb3b9203
xnnxrt-promax <github@serenely.day>
2025-04-06 00:34:38 +0700

    server.js

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.get('/api', (req, res) => {
     res.json({
         message: '',
         version: 'Serenely Day 2.5.3 🟢',
-        festival: '💦 Songkran',
+        festival: '💦 Songkran Day',
         az_discord_profile: 'https://discord.com/users/1119676900462313572',
         doc: 'https://110.ovdc.xyz',
         aungpao_link: 'https://pornhub.org',
